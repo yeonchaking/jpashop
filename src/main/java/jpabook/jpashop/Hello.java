7f6ea83667b3dade98364c991cb961ffacb264cd
연제경 <yllh325@gmail.com>
2023-09-08 03:00:48 +0900

    jdbc connect

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpabook.jpashop;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class Hello {
+
+    private String data;
+}
